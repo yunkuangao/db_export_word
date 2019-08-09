b0e79ec5b812c49e27f77f140e577080d3c210e3
yunkuangao <b412034066@163.com>
2019-08-09 16:03:45 +0800

    修改mysql和sqlserver数据库的sql，三大数据库都可以使用 优化访问方式，http://localhost:8080即可访问

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
-## 来源于zhaowenjie<1513041820@qq.com>
+## 将数据库表结构进行word输出（支持oracle,mysql,sqlserver）
+
+
 
 ## 将数据库表结构进行word输出（现只支持oracle）
 
-## 访问地址http://localhost:9999/dbExport/web/index.html
+##访问地址http://localhost:8080
 
 ## 预计增加功能
 #### mysql sqlserver的数据库增加，高可配数据表导出信息
@@ -10,3 +12,10 @@
 ## 修改了显示内容，增加主键列
 #### 修改时间 2019年8月6日 12点17分
 #### word内能够显示是否为主键
+
+## 输出结构
+表名 表说明
+| 列名 | 数据类型 | 数据长度 | 是否为空 | 是否主键 | 默认值 | 备注 |
+| ---- |   ----   |   ----   | ----  | ----   |  ----  | ----   |
+
+#### 旧式访问地址，不可使用 ~~http://localhost:8080/dbExport/web/index.html~~ 
--- src/main/java/com/pomzwj/DatabaseExportApplication.java
+++ src/main/java/com/pomzwj/DatabaseExportApplication.java
@@ -2,14 +2,23 @@ package com.pomzwj;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * @author zhaowenjie<1513041820@qq.com>
  */
 @SpringBootApplication
+@Controller
 public class DatabaseExportApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DatabaseExportApplication.class, args);
+	}
+
+	@GetMapping("/")
+	String hello(){
+		return "redirect:web/index.html";
 	}
 }
