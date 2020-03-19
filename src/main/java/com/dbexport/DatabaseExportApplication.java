325c5821ecac19063e3b175eca70d244371c11d6
yunkuangao <b412034066@163.com>
2020-03-19 14:43:10 +0800

    使用tomcat9部署，将模板文件内置到项目中，不在从其他位置获取

--- src/main/java/com/dbexport/DatabaseExportApplication.java
+++ src/main/java/com/dbexport/DatabaseExportApplication.java
@@ -2,7 +2,6 @@ package com.dbexport;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.stereotype.Controller;
@@ -20,10 +19,10 @@ public class DatabaseExportApplication extends SpringBootServletInitializer {
         SpringApplication.run(DatabaseExportApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-        return builder.sources(this.getClass());
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+//        return builder.sources(this.getClass());
+//    }
 
     /**
      * 给出一个默认页面
